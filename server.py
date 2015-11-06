5b6400a158dffe01a433ae9a853a937e5e6adbed
mehrik <sobreo.m@gmail.com>
2015-11-06 09:52:24 -0800

    added a reset feature to reset gold amout

--- server.py
+++ server.py
@@ -13,33 +13,37 @@ def index():
 		session['log']
 	except:
 		session['gold'] = 0
-		session['log'] = ()
+		session['log'] = []
 	return render_template('index.html')
 
 @app.route('/process_money', methods=['POST'])
 def process():
-	if request.form['action'] == 'farm':
+	date = datetime.now().strftime('(%Y/%m/%d %r)')
+	if request.form['action'] == 'reset':
+		session['gold'] = 0
+		#want to add a way to clear the activities box
+	elif request.form['action'] == 'farm':
 		goldearned = random.randrange(10,21)
 		session['gold'] += goldearned
-		consolelog.insert(0, 'Earned' + ' ' +str(goldearned)+ ' ' + 'golds from the farm!')
+		consolelog.insert(0, 'Earned' + ' ' +str(goldearned)+ ' ' + 'golds from the farm! ' + date)
 	elif request.form['action'] == 'cave':
 		goldearned = random.randrange(5,11)
 		session['gold'] += goldearned
-		consolelog.insert(0, 'Earned' + ' ' +str(goldearned)+ ' ' + 'golds from the cave!')
+		consolelog.insert(0, 'Earned' + ' ' +str(goldearned)+ ' ' + 'golds from the cave! ' + date)
 	elif request.form['action'] == 'house':
 		goldearned = random.randrange(2,6)
 		session['gold'] += goldearned
-		consolelog.insert(0, 'Earned' + ' ' +str(goldearned)+ ' ' + 'golds from the farm!')
+		consolelog.insert(0, 'Earned' + ' ' +str(goldearned)+ ' ' + 'golds from the house! ' + date)
 	else:
 		gamble = round(random.random())
 		if gamble == 0:
 			goldearned = random.randrange(0,51)
 			session['gold'] += goldearned
-			consolelog.insert(0, 'Entered a casino and won' + ' ' +str(goldearned)+ ' ' + 'golds... YAY!')
+			consolelog.insert(0, 'Entered a casino and won' + ' ' +str(goldearned)+ ' ' + 'golds... YAY! ' + date)
 		else:
 			goldearned = random.randrange(0,51)
 			session['gold'] -= goldearned
-			consolelog.insert(0, 'Entered a casino and lost' + ' ' +str(goldearned)+ ' ' + 'golds... ouch..')
+			consolelog.insert(0, 'Entered a casino and lost' + ' ' +str(goldearned)+ ' ' + 'golds... ouch.. ' + date)
 			if session['gold'] < 0:
 				session['gold'] = 0
 	session['log'] = consolelog
