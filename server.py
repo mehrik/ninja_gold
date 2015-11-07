8de5c9e03d9de0b84718c7d89715800b070e273f
mehrik <sobreo.m@gmail.com>
2015-11-07 13:01:00 -0800

    factored out some code in server.py and added comments for readability

--- server.py
+++ server.py
@@ -19,34 +19,38 @@ def index():
 @app.route('/process_money', methods=['POST'])
 def process():
 	date = datetime.now().strftime('(%Y/%m/%d %r)')
-	if request.form['action'] == 'reset':
-		session.pop('gold')
-		session.pop('log')
-	elif request.form['action'] == 'farm':
+	goldearned = 0;
+
+	#Determines how much gold is earned or lost
+	if request.form['action'] == 'farm':
 		goldearned = random.randrange(10,21)
-		session['gold'] += goldearned
-		consolelog.insert(0, 'Earned' + ' ' +str(goldearned)+ ' ' + 'golds from the farm! ' + date)
 	elif request.form['action'] == 'cave':
 		goldearned = random.randrange(5,11)
-		session['gold'] += goldearned
-		consolelog.insert(0, 'Earned' + ' ' +str(goldearned)+ ' ' + 'golds from the cave! ' + date)
 	elif request.form['action'] == 'house':
 		goldearned = random.randrange(2,6)
-		session['gold'] += goldearned
-		consolelog.insert(0, 'Earned' + ' ' +str(goldearned)+ ' ' + 'golds from the house! ' + date)
-	else:
+	elif request.form['action'] == 'casino':
 		gamble = round(random.random())
-		if gamble == 0:
-			goldearned = random.randrange(0,51)
-			session['gold'] += goldearned
+		goldearned = random.randrange(-50,51)
+		if goldearned > 0:
 			consolelog.insert(0, 'Entered a casino and won' + ' ' +str(goldearned)+ ' ' + 'golds... YAY! ' + date)
 		else:
-			goldearned = random.randrange(0,51)
-			session['gold'] -= goldearned
-			consolelog.insert(0, 'Entered a casino and lost' + ' ' +str(goldearned)+ ' ' + 'golds... ouch.. ' + date)
-			if session['gold'] < 0:
-				session['gold'] = 0
+			consolelog.insert(0, 'Entered a casino and lost' + ' ' +str(goldearned * -1)+ ' ' + 'golds... ouch.. ' + date)
+	session['gold'] += goldearned
+
+	#If gold goes below, zero, it will stay at 0
+	if session['gold'] < 0:
+		session['gold'] = 0
+
+	#Print this line if the gold earned was not from casino
+	if goldearned > 0 and request.form['action'] != 'casino':
+		consolelog.insert(0, 'Earned' + ' ' +str(goldearned)+ ' ' + 'golds from the ' + request.form['action'] +'! ' + date)
 	session['log'] = consolelog
+
+	#Allows user to reset their activities log and gold amount
+	if request.form['action'] == 'reset':
+		session['gold'] = 0
+		del session['log'][:]
+
 	print session['log']
  	return redirect('/')
 
